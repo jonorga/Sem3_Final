6c4fe25dca4d1c891f98414f837390966898b7f6
jonorgan <jonorgan26@gmail.com>
2023-12-01 14:43:10 -0500

    Progress on java final

--- Java/cs-665-project-template-main/src/main/java/edu/bu/met/cs665/ParseCSVTask.java
+++ Java/cs-665-project-template-main/src/main/java/edu/bu/met/cs665/ParseCSVTask.java
@@ -2,25 +2,17 @@
  * Name: Jon Organ
  * Course: CS-665 Software Designs & Patterns
  * Date: 11/23/2023
- * File Name: CountTask.java
+ * File Name: ParseCSVTask.java
  * Description: This file contains the abstract Customer class. 
  */
  
 package music_recommender;
 
-public class Worker implements Runnable {
-    private final Task task;
-
-    public Worker(final Task task) {
-        this.task = task;
-    }
+public class ParseCSVTask extends Task {
+   
 
     /**
      * This method gets the email from the create email behavior
      * @return String
      */
-    @Override
-    public void run() {
-
-    }
 }
--- Java/cs-665-project-template-main/src/test/java/edu/bu/met/cs665/JTest.java
+++ Java/cs-665-project-template-main/src/test/java/edu/bu/met/cs665/JTest.java
@@ -3,16 +3,30 @@ package test;
 import music_recommender.*;
 import static org.junit.Assert.assertEquals;
 import org.junit.Test;
-import java.io.FileReader;  
-//import opencsv.CSVReader;
-
+import java.io.FileReader;
+import java.util.*;
+import java.util.concurrent.*;
 
 public class JTest {
 
 	@Test
 	public void TestBusinessCustomer() {
-		CSVReader reader;
+		Object tasks = List.of(
+	        new ParseCSVTask());
+		Object executor = Executors.newFixedThreadPool(3);
+
+
+		//tasks.stream().map(Worker::new).forEach(executor::execute);
+    	// All tasks were executed, now shutdown
+    	//executor.shutdown();
+
+
+    	
 		/*
+		var tasks = List.of(
+	        new PotatoPeelingTask(3),
+	        new PotatoPeelingTask(5));
+		
 		Customer testCase = new Customer();
 		testCase.SetCustomerType(new BusinessEmail());
 		assertEquals("To whom it may concern, we are reaching out regarding this business email.", 
